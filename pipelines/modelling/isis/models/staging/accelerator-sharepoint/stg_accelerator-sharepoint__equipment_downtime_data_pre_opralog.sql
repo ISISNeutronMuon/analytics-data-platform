078f2e99cc4e4ab2095a93c77d6f2d96b60bb317
Martyn Gigg <martyn.gigg@gmail.com>
2025-08-04 18:00:11 +0100

    Join opralog downtime records with archived records in spreadsheet

--- pipelines/modelling/isis/models/staging/accelerator-sharepoint/stg_accelerator-sharepoint__equipment_downtime_data_pre_opralog.sql
+++ pipelines/modelling/isis/models/staging/accelerator-sharepoint/stg_accelerator-sharepoint__equipment_downtime_data_pre_opralog.sql
@@ -6,41 +6,39 @@ with source as (
 
 ),
 
+cropped as (
+
+    select
+        equipment,
+        downtime_minutesx,
+        -- fault_date column has a full timestamp after the Opralog epoch a mix of timestamp
+        -- with date and 00:00:00 portion or just the date. Chop the date out
+        substring(fault_date, 1, 10) as fault_date_str,
+        -- keep the time portion of any possible string
+        substring(fault_time, -8) as fault_time_str,
+        {{ identifier('group') }},
+        fault_description,
+        managerscomments
+
+    from source
+    where date(substring(fault_date, 1, 10)) < date('{{ var("opralog_epoch_date") }}')
+
+),
+
 renamed as (
 
     select
         equipment,
         downtime_minutesx as downtime_mins,
-        -- fault_date column has a full timestamp after the Opralog epoch but only the date
-        -- before the epoch.
-        case
-          when fault_date like '%T%' then from_iso8601_timestamp(fault_date)
-          else with_timezone(to_timestamp(fault_date, 'yyyy-mm-dd hh24:mi:ss'), 'Europe/London')
-        end as fault_date,
+        date(fault_date_str) as fault_date,
         -- fault_time is just a time before the opralog_epoch but a full timestamp after
-        fault_time,
+        cast(parse_datetime(fault_date_str || ' ' || fault_time_str, 'yyyy-MM-dd HH:mm:ss') as timestamp(6)) at time zone 'UTC' as fault_occurred_at,
         {{ identifier('group') }},
         fault_description,
         managerscomments as managers_comments
 
-    from source
-
-),
-
-cropped as (
-
-    select
-        equipment,
-        downtime_mins,
-        date(fault_date) as fault_date,
-        cast(parse_datetime(cast(date(fault_date) as varchar) || ' ' || fault_time, 'yyyy-MM-dd HH:mm:ss') as timestamp(6)) as fault_occurred_at,
-        "group",
-        fault_description,
-        managers_comments
-
-    from renamed
-    where fault_date < date('{{ var("opralog_epoch") }}')
+    from cropped
 
 )
 
-select * from cropped
+select * from renamed
--- pipelines/modelling/isis/models/staging/opralogweb/stg_opralogweb__user_entries.sql
+++ pipelines/modelling/isis/models/staging/opralogweb/stg_opralogweb__user_entries.sql
@@ -1,13 +1,23 @@
 with
 
 source as (
+
   select * from {{ source('src_opralogweb', 'user_entries') }}
+
+),
+
+renamed as (
+
+  select
+
+      entry_id,
+      entry_timestamp as fault_occurred_at,
+      cast({{ identifier("entry_timestamp") }} as date) as fault_date,
+      trim(additional_comment) as fault_description
+
+  from source
+  where entry_timestamp >= from_iso8601_timestamp('{{ var("opralog_epoch_date") }}')
+
 )
 
-select
-    entry_id,
-    entry_timestamp as fault_occurred_at,
-    cast({{ identifier("entry_timestamp") }} as date) as fault_date,
-    trim(additional_comment) as fault_description
-from
-  source
+select * from renamed
