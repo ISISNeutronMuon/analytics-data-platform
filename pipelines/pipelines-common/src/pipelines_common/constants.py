8848afb03b41fc6d0cf1544664e1661bb4634eb1
Martyn Gigg <martyn.gigg@gmail.com>
2025-08-07 15:25:55 +0100

    chore(pipelines-common): Remove unused pipeline-common code (#124)
    ### Summary
    
    Many functions had become unused in the pipelines-common package. Remove
    before attempting to write more unit tests.
    
    Fixes #122

--- pipelines/pipelines-common/src/pipelines_common/constants.py
+++ pipelines/pipelines-common/src/pipelines_common/constants.py
@@ -1,12 +1,4 @@
 """A set of constants shared by all pipelines"""
 
-# Date/time
-MICROSECONDS_PER_SEC = 1_000_000
-MICROSECONDS_STR = "us"
-MICROSECONDS_STR_INFLUX = "u"
-
 # Pipeline
-DATASET_NAME_PREFIX_SRCS = "src_"
-
-# Catalog
-CATALOG_NAME: str = "isis"
+SOURCE_DATASET_NAME_PREFIX = "src_"
