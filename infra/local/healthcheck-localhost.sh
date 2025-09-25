c6b5bdb48278420d39a0f53aefb739597301d721
Martyn Gigg <martyn.gigg@gmail.com>
2025-09-25 14:11:13 +0100

    Remove dependency on grep in healtcheck

--- infra/local/healthcheck-localhost.sh
+++ infra/local/healthcheck-localhost.sh
@@ -2,23 +2,25 @@
 # Uses the net-redirections feature in bash to send a request to a http endpoint on localhost
 # Opens a file descriptor, redirects it to the specified port and performs a HTTP/GET request to
 # the given endpoint.
-set -eu
 
 # arg processing
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 port endpoint [grep_success_pattern]"
+  echo "Usage: $0 port endpoint [success_pattern]"
   echo
   echo "Send a GET request to an endpoint on port."
   echo
-  echo "  grep_success_pattern: specifies the pattern that the output must contain to be considered a success (default: 200 OK)"
+  echo "  success_pattern: specifies the pattern that the output must contain to be considered a success (default: 200 OK)"
   exit 1
 fi
-grep_success_pattern=${3:-200 OK}
+success_pattern=${3:-200 OK}
 
 # run GET
 exec 3<>/dev/tcp/127.0.0.1/"$1"
+test $? -eq 0 || exit 1
+
 echo -e "GET $2 HTTP/1.1
 host: 127.0.0.1:$1
 " >&3
 
-timeout 1 cat <&3 | grep '200 OK' || exit 1
+response=$(timeout 1 cat <&3)
+[[ "$response" == *"$success_pattern"* ]] || exit 1
