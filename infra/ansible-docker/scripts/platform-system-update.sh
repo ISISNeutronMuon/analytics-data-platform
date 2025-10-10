9267a79f45cb49e64936865be320ba99724b8a85
Martyn Gigg <martyn.gigg@gmail.com>
2025-10-10 09:20:33 +0100

    Enable packages update for elt node

--- infra/ansible-docker/scripts/platform-system-update.sh
+++ infra/ansible-docker/scripts/platform-system-update.sh
@@ -15,7 +15,7 @@ function system_update() {
 test -f $INVENTORY || fail_with_message "$INVENTORY must exist in current directory ($PWD)"
 
 # Do updates in a top-down order
-# system_update elt
+system_update elt
 system_update superset_farm
 system_update trino
 system_update lakekeeper
