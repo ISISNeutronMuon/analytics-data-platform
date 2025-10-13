23b919649c46476af4dfb6208e892bc6794f9c18
Martyn Gigg <martyn.gigg@gmail.com>
2025-10-13 12:49:07 +0100

    Finish bootstrapping Keycloak

--- infra/local/keycloak/bootstrap.sh
+++ infra/local/keycloak/bootstrap.sh
@@ -1,50 +1,76 @@
 #!/bin/bash
-set -u
+set -ux
 
 KC_ADM=/opt/keycloak/bin/kcadm.sh
 KC_SERVER=http://keycloak:8080/auth
 
-
-echo "Creating realms and clients"
+# authenticate
 $KC_ADM config credentials \
-  --server $KC_SERVER \
+  --server "$KC_SERVER" \
   --realm master \
-  --user $KC_BOOTSTRAP_ADMIN_USERNAME \
-  --password $KC_BOOTSTRAP_ADMIN_PASSWORD
+  --user "$KC_BOOTSTRAP_ADMIN_USERNAME" \
+  --password "$KC_BOOTSTRAP_ADMIN_PASSWORD"
 
-# realm
+####################
+# realms
+####################
 $KC_ADM create realms \
-  --set realm=$KC_REALM_NAME \
-  --set enabled=true \
-  --output
+  --set realm="$KC_REALM_NAME" \
+  --set enabled=true
 
+####################
 # client scopes
-read -d '' lakekeeper_aud_mapper_json << EOF
-[{
-  "name": "Add lakekeeper audience",
+####################
+$KC_ADM create client-scopes \
+  --target-realm "$KC_REALM_NAME" \
+  --file - << EOF
+{
+  "name": "lakekeeper",
+  "description": "Add aud=lakekeeper scope",
   "protocol": "openid-connect",
-  "protocolMapper": "oidc-audience-mapper",
-  "consentRequired": false,
-  "config": {
-    "id.token.claim": "false",
-    "lightweight.claim": "false",
-    "access.token.claim": "true",
-    "introspection.token.claim": "true",
-    "included.custom.audience": "lakekeeper"
-  }
-}]
+  "protocolMappers": [
+    {
+      "name": "Add Lakekeeper audience",
+      "protocol": "openid-connect",
+      "protocolMapper": "oidc-audience-mapper",
+      "config": {
+        "id.token.claim": "false",
+        "introspection.token.claim": "true",
+        "access.token.claim": "true",
+        "included.custom.audience": "lakekeeper"
+      }
+    }
+  ]
+}
 EOF
 
-$KC_ADM create client-scopes \
-  --target-realm $KC_REALM_NAME \
-  --set name=lakekeeper \
-  --set protocol=openid-connect \
-  --set description="Lakekeeper scope" \
-  --set protocolMappers="$lakekeeper_aud_mapper_json"
+####################
+# Clients
+####################
+$KC_ADM create clients \
+  --target-realm "$KC_REALM_NAME" \
+  --set clientId=lakekeeper \
+  --set publicClient=true \
+  --set 'redirectUris=["*"]' \
+  --set 'optionalClientScopes=["lakekeeper", "address", "phone", "offline_access", "organization", "microprofile-jwt"]' \
+  --set 'attributes={ "access.token.lifespan": 3600 }'
 
-# clients
-# $KC_ADM create clients \
-#   --target-realm $KC_REALM_NAME \
-#   --set clientId=lakekeeper \
-#   --set secret=s3cr3t \
-#   --
+$KC_ADM create clients \
+  --target-realm "$KC_REALM_NAME" \
+  --set clientId=machine-infra \
+  --set publicClient=false \
+  --set serviceAccountsEnabled=true \
+  --set 'redirectUris=["*"]' \
+  --set 'optionalClientScopes=["lakekeeper", "address", "phone", "offline_access", "organization", "microprofile-jwt"]' \
+  --set 'attributes={ "access.token.lifespan": 600 }' \
+  --set 'secret=s3cr3t'
+
+$KC_ADM create clients \
+  --target-realm "$KC_REALM_NAME" \
+  --set clientId=trino \
+  --set publicClient=false \
+  --set serviceAccountsEnabled=true \
+  --set 'redirectUris=["*"]' \
+  --set 'optionalClientScopes=["lakekeeper", "address", "phone", "offline_access", "organization", "microprofile-jwt"]' \
+  --set 'attributes={ "access.token.lifespan": 600 }' \
+  --set 'secret=s3cr3t'
