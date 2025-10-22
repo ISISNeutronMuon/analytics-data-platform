bd970393e2d04d032a1fa3f6ac64ffa46111585c
Martyn Gigg <martyn.gigg@gmail.com>
2025-10-22 14:47:44 +0100

    Create a superset user for REST api logins.
    The superset rest api only accepts HSA256-sgined jwt tokens and
    Keycloak uses RSA256. Create a user for rest api logins so we
    can use Superset to generate the tokens.

--- infra/local/superset/docker/docker-init.sh
+++ infra/local/superset/docker/docker-init.sh
@@ -6,15 +6,13 @@ set -e
 #
 /app/docker/docker-bootstrap.sh
 
-STEP_CNT=2
-
 echo_step() {
 cat <<EOF
 
 ######################################################################
 
 
-Init Step ${1}/${STEP_CNT} [${2}] -- ${3}
+Init [${1}] -- ${2}
 
 
 ######################################################################
@@ -23,28 +21,30 @@ EOF
 }
 
 # Initialize the database
-echo_step "1" "Starting" "Applying DB migrations"
+echo_step "Starting" "Applying DB migrations"
 superset db upgrade
-echo_step "1" "Complete" "Applying DB migrations"
+echo_step "Complete" "Applying DB migrations"
 
-# Create an admin user
-# echo_step "2" "Starting" "Setting up admin user admin "
-# superset fab create-admin \
-#               --username "$SUPERSET_ADMIN_USER" \
-#               --firstname "$SUPERSET_ADMIN_FIRSTNAME" \
-#               --lastname "$SUPERSET_ADMIN_LASTNAME" \
-#               --email "$SUPERSET_ADMIN_EMAIL" \
-#               --password "$SUPERSET_ADMIN_PASSWORD"
-# echo_step "2" "Complete" "Setting up admin user"
+# Create an admin user (if supplied)
+if [ -n "$SUPERSET_ADMIN_USER" ]; then
+  echo_step "Starting" "Setting up admin user"
+  superset fab create-admin \
+                --username "$SUPERSET_ADMIN_USER" \
+                --firstname "$SUPERSET_ADMIN_FIRSTNAME" \
+                --lastname "$SUPERSET_ADMIN_LASTNAME" \
+                --email "$SUPERSET_ADMIN_EMAIL" \
+                --password "$SUPERSET_ADMIN_PASSWORD"
+  echo_step "Complete" "Setting up admin user"
+fi
 
 # Create default roles and permissions
-echo_step "2" "Starting" "Setting up roles and perms"
+echo_step "Starting" "Setting up roles and perms"
 superset init
-echo_step "2" "Complete" "Setting up roles and perms"
+echo_step "Complete" "Setting up roles and perms"
 
 # Create a database connection
-# echo_step "4" "Starting" "Setting up Iceberg catalog connection"
-# superset set-database-uri \
-#   --database_name "$SUPERSET_DB_SCHEMA_NAME" \
-#   --uri "trino://superset:adppassword@trino:8080/$SUPERSET_DB_SCHEMA_NAME"
-# echo_step "4" "Complete" "Setting up data source database connection"
+echo_step "Starting" "Setting up Iceberg catalog connection"
+superset set-database-uri \
+  --database_name "$SUPERSET_DB_SCHEMA_NAME" \
+  --uri "trino://superset:s3cr3t@analytics.localdev:58443/$SUPERSET_DB_SCHEMA_NAME?engine_params={\"verify\": false}"
+echo_step "Complete" "Setting up data source database connection"
