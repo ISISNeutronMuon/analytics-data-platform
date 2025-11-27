b75d49bb9340ebfaa1ce8cdf13d689a840fd27f2
Martyn Gigg <martyn.gigg@gmail.com>
2025-11-27 14:41:21 +0000

    Remove duplicated constants

--- elt-common/src/elt_common/dlt_destinations/pyiceberg/type_mapping.py
+++ elt-common/src/elt_common/dlt_destinations/pyiceberg/type_mapping.py
@@ -1,5 +1,3 @@
-from typing import Dict
-
 from elt_common.dlt_destinations.pyiceberg.helpers import (
     dlt_type_to_iceberg,
     iceberg_to_dlt_type,
@@ -10,10 +8,6 @@ from dlt.common.destination.typing import PreparedTableSchema
 from dlt.common.schema.typing import TColumnSchema, TColumnType
 from dlt.common.utils import without_none
 from dlt.destinations.type_mapping import TypeMapperImpl
-
-
-TIMESTAMP_PRECISION_TO_UNIT: Dict[int, str] = {0: "s", 3: "ms", 6: "us", 9: "ns"}
-UNIT_TO_TIMESTAMP_PRECISION: Dict[str, int] = {v: k for k, v in TIMESTAMP_PRECISION_TO_UNIT.items()}
 
 
 class PyIcebergTypeMapper(TypeMapperImpl):
