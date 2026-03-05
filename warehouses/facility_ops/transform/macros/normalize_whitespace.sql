e7e9ebad1400e14d023711a3e447d8db9197cdc8
Martyn Gigg <martyn.gigg@gmail.com>
2026-03-05 09:13:33 +0000

    Add macros to facility_ops

---
+++ warehouses/facility_ops/transform/macros/normalize_whitespace.sql
@@ -0,0 +1,8 @@
+-- Removes leading and trailing whitespace and replaces any multiple-spaces with a single space
+{%- macro normalize_whitespace(text_col) -%}
+  {{ return(adapter.dispatch('normalize_whitespace')(text_col)) }}
+{% endmacro %}
+
+{%- macro default__normalize_whitespace(text_col) -%}
+  regexp_replace(trim({{ text_col }}), '\s+', ' ')
+{%- endmacro -%}
