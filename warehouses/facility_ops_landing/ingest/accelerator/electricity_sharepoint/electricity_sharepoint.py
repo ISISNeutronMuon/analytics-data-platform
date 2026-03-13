7beda154352d4aa30f98249e3eebd72d097bd4be
Martyn Gigg <martyn.gigg@gmail.com>
2026-03-13 17:13:22 +0000

    Rework electricity sharepoint
    Allow parsing a file that is concatenated versions of the
    single-file format. Removed the skip_rows option in favour
    of infering the start position.

--- warehouses/facility_ops_landing/ingest/accelerator/electricity_sharepoint/electricity_sharepoint.py
+++ warehouses/facility_ops_landing/ingest/accelerator/electricity_sharepoint/electricity_sharepoint.py
@@ -13,13 +13,15 @@ import concurrent.futures
 import io
 import os
 import pathlib
-from typing import Iterator, Optional
+from typing import Iterator, Optional, Sequence
 
 import dlt
 import dlt.common.logger as logger
 from dlt.sources import TDataItems
 import pendulum
 import pandas as pd
+import pytz
+
 from elt_common.cli import cli_main
 from elt_common.dlt_sources.m365 import (
     sharepoint,
@@ -36,6 +38,20 @@ ONE_DAY_SECS = 24 * 60 * 60
 PIPELINE_NAME = "electricity_sharepoint"
 RDM_TIMEZONE = "Europe/London"
 SITE_URL = "https://stfc365.sharepoint.com/sites/ISISSustainability"
+
+# File information
+# The file can contain a record from a single export or multiple records combined. Each record starts with
+# a header:
+
+# Site Information:
+# RAL ISIS RDM
+# RAL ISIS RDM
+# Controller: ISIS
+# Controller description: ISIS Energy Totals
+# Status: Online
+
+# Time	Date	ISIS Elec Total Power {MW}
+CSV_HEADER_ANCHOR = "time"
 
 
 def effective_max_workers(max_workers: int | None) -> int:
@@ -55,81 +71,75 @@ def to_utc(ts: pd.Series) -> pd.Series:
     return ts.dt.tz_localize(RDM_TIMEZONE).dt.tz_convert("UTC")
 
 
-def read_power_consumption_csv(
-    file_content: io.BytesIO, skip_rows: int
-) -> pd.DataFrame:
+def read_power_consumption_csv(file_content: io.BytesIO) -> pd.DataFrame:
     """Read csv-formatted power consumption records. This can return None if there is not data in the file.
 
-    The file can contain a record from a single export or multiple records combined. Each record starts with
-    a header:
-
-    Site Information:
-    RAL ISIS RDM
-    RAL ISIS RDM
-    Controller: ISIS
-    Controller description: ISIS Energy Totals
-    Status: Online
-
-    Time	Date	ISIS Elec Total Power {MW}
     The Date & Time columns together to create a single DateTime column.
     """
+
+    def csv_to_df(lines: Sequence[str]) -> pd.DataFrame:
+        return pd.read_csv(io.StringIO("\n".join(lines)))
+
     metadata_anchor = "site information"
-    csv_header_anchor = "time,date"
     sections, current_lines, in_data = [], [], False
     for line in file_content.getvalue().decode().splitlines():
         line = line.strip()
         line_lower = line.lower()
 
-        if line_lower.startswith(csv_header_anchor):
+        if line_lower.startswith(CSV_HEADER_ANCHOR):
             # Save any previous section
             if current_lines:
-                sections.append("\n".join(current_lines))
-            # Start new section with header
-            current_lines = [csv_header_anchor]
+                sections.append(csv_to_df(current_lines))
+            # start new section with header
+            current_lines = [line]
             in_data = True
 
         elif in_data:
             if line_lower.startswith(metadata_anchor):
                 # Metadata block
                 in_data = False
-            elif line == "":
-                # Skip blank lines and metadata lines
-                pass
+                continue
             else:
                 current_lines.append(line)
 
-    # # Don't forget the last section
-    # if current_lines:
-    #     sections.append("\n".join(current_lines))
+    # the last section
+    if current_lines:
+        sections.append(csv_to_df(current_lines))
 
-    # # Parse each section and concatenate
-    # dfs = [pd.read_csv(StringIO(s)) for s in sections]
+    # concatenate
+    df = pd.concat(sections, ignore_index=True)
 
-    # df = pd.read_csv(file_content, skiprows=skip_rows)
-    # df["DateTime"] = to_utc(
-    #     pd.to_datetime(df["Date"] + " " + df["Time"], format="%d/%m/%y %H:%M:%S")  # type: ignore
-    # )
-    # return df.drop(["Date", "Time"], axis=1)
+    # clean up column name (strip any whitespace)
+    df.columns = df.columns.str.strip()
+    df["DateTime"] = to_utc(
+        pd.to_datetime(df["Date"] + " " + df["Time"], format="%d/%m/%y %H:%M:%S")  # type: ignore
+    )
+    return df.drop(["Date", "Time"], axis=1)
 
 
-def read_power_consumption_excel(
-    file_content: io.BytesIO, skip_rows: int
-) -> pd.DataFrame:
+def read_power_consumption_excel(file_content: io.BytesIO) -> pd.DataFrame:
     """Read an excel-formatted power consumption record.
 
     Expected columns: Time, Total Power (MW)
     The Time column is renamed DateTime for consistency.
     """
-    df = pd.read_excel(file_content, engine=EXCEL_ENGINE, skiprows=skip_rows)
-    df = df.rename(columns={"Time": "DateTime"})
-    df["DateTime"] = to_utc(df["DateTime"])
+    df = pd.read_excel(file_content, engine=EXCEL_ENGINE)
+    # trim out header:
+    #  - second column is all NaN until header
+    #  - header itself starts with text defined in CSV_HEADER_ANCHOR
+    # we want all rows where True
+    df = df[
+        df.iloc[:, 1].notna()
+        & ~df.iloc[:, 0].str.contains(f"(?i){CSV_HEADER_ANCHOR}", na=False)
+    ]
+    df = df.set_axis(["DateTime", "ISIS Power MW"], axis=1)
+    df["DateTime"] = to_utc(pd.to_datetime(df["DateTime"], format="ISO8601"))
     return df
 
 
 @dlt.transformer(section="m365")
 def extract_content_and_read(
     items: Iterator[M365DriveItem],
-    skip_rows: int = dlt.config.value,
     max_workers: Optional[int] = dlt.config.value,
 ) -> Iterator[TDataItems]:
     """Extracts the file content and reads it assuming it is a .csv or a .xlsx file
@@ -138,7 +148,6 @@ def extract_content_and_read(
     processing.
 
     :param items: An iterator of dicts describing the file content
-    :param skip_rows: Number of rows in the csv/xlsx files to skip
     :param max_workers (optional): How many threads to use to process the files.
                                    Defaults to a maximum defined by concurrent.futures.ThreadPoolExecutor
     """
@@ -152,14 +161,14 @@ def extract_content_and_read(
         try:
             match pathlib.Path(file_name).suffix:
                 case ".csv":
-                    df = read_power_consumption_csv(file_content, skip_rows)
+                    df = read_power_consumption_csv(file_content)
                 case ".xlsx":
-                    df = read_power_consumption_excel(file_content, skip_rows)
+                    df = read_power_consumption_excel(file_content)
                 case _:
                     raise RuntimeError(f"Unsupported file extension in '{file_name}'")
-        except Exception as exc:
-            logger.error(
-                f"'Error loading {file_name} ({len(file_bytes)} bytes)'. File skipped.\nDetails: {str(exc)}"
+        except pytz.exceptions.AmbiguousTimeError as exc:
+            logger.warning(
+                f"'Error loading {file_name} ({len(file_bytes)} bytes)'. There will be gaps in the data.\nDetails: {str(exc)}"
             )
             return None
 
