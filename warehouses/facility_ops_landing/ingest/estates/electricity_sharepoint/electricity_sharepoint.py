a79373e2ff810d1b5d23e488421f4d116e550395
Martyn Gigg <martyn.gigg@gmail.com>
2026-03-16 17:26:42 +0000

    Remove dlt incremental loading that is confusing
    Load the new batch daily files

--- warehouses/facility_ops_landing/ingest/estates/electricity_sharepoint/electricity_sharepoint.py
+++ warehouses/facility_ops_landing/ingest/estates/electricity_sharepoint/electricity_sharepoint.py
@@ -13,13 +13,14 @@ import concurrent.futures
 import io
 import os
 import pathlib
-from typing import Iterator, Optional, Sequence
+from typing import Iterator, Sequence
 
 import dlt
 import dlt.common.logger as logger
 from dlt.sources import TDataItems
 import pendulum
 import pandas as pd
+import pyarrow.compute as pc
 import pytz
 
 from elt_common.cli import cli_main
@@ -27,17 +28,18 @@ from elt_common.dlt_sources.m365 import (
     sharepoint,
     M365DriveItem,
 )
+from elt_common.dlt_destinations.pyiceberg.helpers import load_iceberg_table
 from elt_common.dlt_destinations.pyiceberg.pyiceberg_adapter import (
     pyiceberg_adapter,
     PartitionTrBuilder,
 )
 
-MAX_WORKERS_DEFAULT = min(8, (os.cpu_count() or 1) + 4)
 EXCEL_ENGINE = "calamine"
+EXCEL_SKIP_ROWS = 7
+MAX_WORKERS = min(8, (os.cpu_count() or 1) + 4)
 ONE_DAY_SECS = 24 * 60 * 60
 PIPELINE_NAME = "electricity_sharepoint"
 RDM_TIMEZONE = "Europe/London"
-SITE_URL = "https://stfc365.sharepoint.com/sites/ISISSustainability"
 
 # File information
 # The file can contain a record from a single export or multiple records combined. Each record starts with
@@ -54,31 +56,40 @@ SITE_URL = "https://stfc365.sharepoint.com/sites/ISISSustainability"
 CSV_HEADER_ANCHOR = "time"
 
 
-def effective_max_workers(max_workers: int | None) -> int:
-    # For high-cpu-count machines the ThreadPoolExecutor default ends up being too high.
-    # It has been observed that too many concurrent threads cause issues with partial file reads from
-    # OneDrive. 8 threads seems to work correctly so clamp this as them maximum...
-
-    return (
-        min(MAX_WORKERS_DEFAULT, max_workers)
-        if max_workers is not None
-        else MAX_WORKERS_DEFAULT
-    )
-
-
 def to_utc(ts: pd.Series) -> pd.Series:
     """Assumes timezone unaware data and converts to UTC"""
     return ts.dt.tz_localize(RDM_TIMEZONE).dt.tz_convert("UTC")
 
 
-def read_power_consumption_csv(file_content: io.BytesIO) -> pd.DataFrame:
+def csv_section_to_df(file_name: str, lines: Sequence[str]) -> pd.DataFrame | None:
+    """Parse csv and return a DataFrame if the times are valid. None if not"""
+    df = pd.read_csv(io.StringIO("\n".join(lines)))
+    # clean up column name (strip any whitespace)
+    df.columns = df.columns.str.strip()
+    try:
+        df["DateTime"] = to_utc(
+            pd.to_datetime(df["Date"] + " " + df["Time"], format="%d/%m/%y %H:%M:%S")  # type: ignore
+        )
+        return df.drop(["Date", "Time"], axis=1)
+    except pytz.exceptions.AmbiguousTimeError as exc:
+        logger.warning(
+            f"'Error loading section of {file_name}'. There will be gaps in the data.\nDetails: {str(exc)}"
+        )
+        return None
+
+
+def read_power_consumption_csv(
+    file_content: io.BytesIO,
+    file_name: str,
+) -> pd.DataFrame | None:
     """Read csv-formatted power consumption records. This can return None if there is not data in the file.
 
     The Date & Time columns together to create a single DateTime column.
     """
 
-    def csv_to_df(lines: Sequence[str]) -> pd.DataFrame:
-        return pd.read_csv(io.StringIO("\n".join(lines)))
+    def _append_if_not_none(seq, df):
+        if df is not None:
+            seq.append(df)
 
     metadata_anchor = "site information"
     sections, current_lines, in_data = [], [], False
@@ -89,7 +100,9 @@ def read_power_consumption_csv(file_content: io.BytesIO) -> pd.DataFrame:
         if line_lower.startswith(CSV_HEADER_ANCHOR):
             # Save any previous section
             if current_lines:
-                sections.append(csv_to_df(current_lines))
+                _append_if_not_none(
+                    sections, csv_section_to_df(file_name, current_lines)
+                )
             # start new section with header
             current_lines = [line]
             in_data = True
@@ -104,17 +117,13 @@ def read_power_consumption_csv(file_content: io.BytesIO) -> pd.DataFrame:
 
     # the last section
     if current_lines:
-        sections.append(csv_to_df(current_lines))
+        _append_if_not_none(sections, csv_section_to_df(file_name, current_lines))
 
     # concatenate
-    df = pd.concat(sections, ignore_index=True)
+    if sections:
+        return pd.concat(sections, ignore_index=True)
 
-    # clean up column name (strip any whitespace)
-    df.columns = df.columns.str.strip()
-    df["DateTime"] = to_utc(
-        pd.to_datetime(df["Date"] + " " + df["Time"], format="%d/%m/%y %H:%M:%S")  # type: ignore
-    )
-    return df.drop(["Date", "Time"], axis=1)
+    return None
 
 
 def read_power_consumption_excel(file_content: io.BytesIO) -> pd.DataFrame:
@@ -123,25 +132,14 @@ def read_power_consumption_excel(file_content: io.BytesIO) -> pd.DataFrame:
     Expected columns: Time, Total Power (MW)
     The Time column is renamed DateTime for consistency.
     """
-    df = pd.read_excel(file_content, engine=EXCEL_ENGINE)
-    # trim out header:
-    #  - second column is all NaN until header
-    #  - header itself starts with text defined in CSV_HEADER_ANCHOR
-    # we want all rows where True
-    df = df[
-        df.iloc[:, 1].notna()
-        & ~df.iloc[:, 0].str.contains(f"(?i){CSV_HEADER_ANCHOR}", na=False)
-    ]
-    df = df.set_axis(["DateTime", "ISIS Power MW"], axis=1)
-    df["DateTime"] = to_utc(pd.to_datetime(df["DateTime"], format="ISO8601"))
+    df = pd.read_excel(file_content, engine=EXCEL_ENGINE, skiprows=EXCEL_SKIP_ROWS)
+    df = df.rename(columns={"Time": "DateTime"})
+    df["DateTime"] = to_utc(df["DateTime"])
     return df
 
 
 @dlt.transformer(section="m365")
-def extract_content_and_read(
-    items: Iterator[M365DriveItem],
-    max_workers: Optional[int] = dlt.config.value,
-) -> Iterator[TDataItems]:
+def extract_content_and_read(items: Iterator[M365DriveItem]) -> Iterator[TDataItems]:
     """Extracts the file content and reads it assuming it is a .csv or a .xlsx file
 
     Combines the Date and Time columns into a DateTime field to simplify incremental
@@ -158,27 +156,18 @@ def extract_content_and_read(
         file_bytes = file_obj.read_bytes()
         file_content = io.BytesIO(file_bytes)
         logger.debug(f"Filename '{file_name}' has size {len(file_bytes)} bytes.")
-        try:
-            match pathlib.Path(file_name).suffix:
-                case ".csv":
-                    df = read_power_consumption_csv(file_content)
-                case ".xlsx":
-                    df = read_power_consumption_excel(file_content)
-                case _:
-                    raise RuntimeError(f"Unsupported file extension in '{file_name}'")
-        except pytz.exceptions.AmbiguousTimeError as exc:
-            logger.warning(
-                f"'Error loading {file_name} ({len(file_bytes)} bytes)'. There will be gaps in the data.\nDetails: {str(exc)}"
-            )
-            return None
+        match pathlib.Path(file_name).suffix:
+            case ".csv":
+                df = read_power_consumption_csv(file_content, file_name)
+            case ".xlsx":
+                df = read_power_consumption_excel(file_content)
+            case _:
+                raise RuntimeError(f"Unsupported file extension in '{file_name}'")
 
-        df["file_name"] = file_obj["file_name"]
         return df
 
     df_batch = None
-    with concurrent.futures.ThreadPoolExecutor(
-        max_workers=effective_max_workers(max_workers)
-    ) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
         future_to_file_item = {
             executor.submit(read_as_dataframe, file_obj): file_obj for file_obj in items
         }
@@ -192,21 +181,61 @@ def extract_content_and_read(
 
 @dlt.resource(merge_key="DateTime", columns={"DateTime": {"nullable": False}})
 def rdm_data(
-    datetime_cur=dlt.sources.incremental(
-        "DateTime",
-        initial_value=pendulum.DateTime.EPOCH,
-    ),
+    site_url: str = dlt.config.value,
+    root_dir: str = dlt.config.value,
+    backfill: bool = False,
 ) -> Iterator[TDataItems]:
-    files = sharepoint(
-        site_url=SITE_URL,
+    if backfill:
+        historic_xl = sharepoint(
+            site_url=site_url,
+            file_glob=f"{root_dir}/**/*.xlsx",
+            extract_content=False,
+        )
+        reader = historic_xl | extract_content_and_read()
+        yield from reader.apply_hints(
+            write_disposition="merge",
+        )
+        historic_csv = sharepoint(
+            site_url=site_url,
+            file_glob=f"{root_dir}/**/*-daily.csv",
+            extract_content=False,
+        )
+        reader = historic_csv | extract_content_and_read()
+        yield from reader.apply_hints(
+            write_disposition="merge",
+        )
+
+    latest_files = sharepoint(
+        site_url=site_url,
+        file_glob=f"{root_dir}/*-ISIS.csv",
         extract_content=False,
-        modified_after=datetime_cur.start_value
-        - pendulum.Duration(seconds=ONE_DAY_SECS),
+        modified_after=get_latest_timestamp(dlt.current.pipeline()),
     )
-    reader = files | extract_content_and_read()
+    reader = latest_files | extract_content_and_read()
     yield from reader.apply_hints(
         write_disposition="merge",
     )
+
+
+def get_latest_timestamp(pipeline: dlt.Pipeline) -> pendulum.DateTime | None:
+    """Retrieve the timestamp loaded into the warehouse"""
+    existing_rdm_data = load_iceberg_table(pipeline, "rdm_data")
+    if existing_rdm_data is None:
+        logger.debug("Peaks table does not exist. No runs have been loaded.")
+        return None
+
+    logger.debug("Peaks table exists, finding latest timestamp")
+    c_datetime = "date_time"
+    scan = existing_rdm_data.scan(selected_fields=(c_datetime,))
+    running_max = None
+    for batch in scan.to_arrow_batch_reader():
+        batch_max = pc.max(batch[c_datetime])
+        if batch_max.is_valid:
+            running_max = (
+                batch_max if running_max is None else pc.max([running_max, batch_max])
+            )
+
+    return pendulum.instance(running_max.as_py()) if running_max is not None else None
 
 
 if __name__ == "__main__":
@@ -215,5 +244,5 @@ if __name__ == "__main__":
         data_generator=pyiceberg_adapter(
             rdm_data, partition=PartitionTrBuilder.year("date_time")
         ),
-        source_domain="accelerator",
+        source_domain="estates",
     )
