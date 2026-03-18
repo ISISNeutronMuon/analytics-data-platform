cbe22d761dc0b95da5ca2c56bf59f67b4061332d
Martyn Gigg <martyn.gigg@gmail.com>
2026-03-18 16:02:38 +0000

    Working impl with partition/sort order support

---
+++ elt-common/src/elt_common/iceberg/partition.py
@@ -0,0 +1,86 @@
+from pyiceberg.partitioning import (
+    UNPARTITIONED_PARTITION_SPEC,
+    PartitionField,
+    PartitionSpec,
+)
+from pyiceberg.schema import Schema
+import pyiceberg.transforms as transforms
+
+PartitionHint = dict[str, str] | None
+
+
+class PartitionTransformation:
+    transform: str
+    """The transform as a string representation understood by pyicberg.transforms.parse_transform., e.g. `bucket[16]`"""
+    column_name: str
+    """Column name to apply the transformation to"""
+
+    def __init__(self, transform: str, column_name: str) -> None:
+        self.transform = transform
+        self.column_name = column_name
+
+
+class PartitionTrBuilder:
+    """Helper class to generate iceberg partition transformations"""
+
+    @staticmethod
+    def identity(column_name: str) -> PartitionTransformation:
+        """Partition by column without an transformation"""
+        return PartitionTransformation(transforms.IDENTITY, column_name)
+
+    @staticmethod
+    def year(column_name: str) -> PartitionTransformation:
+        """Partition by year part of a date or timestamp column."""
+        return PartitionTransformation(transforms.YEAR, column_name)
+
+    @staticmethod
+    def month(column_name: str) -> PartitionTransformation:
+        """Partition by month part of a date or timestamp column."""
+        return PartitionTransformation(transforms.MONTH, column_name)
+
+    @staticmethod
+    def day(column_name: str) -> PartitionTransformation:
+        """Partition by day part of a date or timestamp column."""
+        return PartitionTransformation(transforms.DAY, column_name)
+
+    @staticmethod
+    def hour(column_name: str) -> PartitionTransformation:
+        """Partition by hour part of a date or timestamp column."""
+        return PartitionTransformation(transforms.HOUR, column_name)
+
+    # NOTE: The following transformations are not currently supported by writing through
+    # pyarrow so they are disabled.
+
+    # @staticmethod
+    # def bucket(n: int, column_name: str) -> PartitionTransformation:
+    #     """Partition by hashed value to n buckets."""
+    #     return PartitionTransformation(f"{transforms.BUCKET}[{n}]", column_name)
+
+    # @staticmethod
+    # def truncate(length: int, column_name: str) -> PartitionTransformation:
+    #     """Partition by value truncated to length."""
+    #     return PartitionTransformation(f"{transforms.TRUNCATE}[{length}]", column_name)
+
+
+def create_partition_spec(partition_hint: PartitionHint, iceberg_schema: Schema) -> PartitionSpec:
+    """Create an Iceberg partition spec for this table if the partition hints
+    have been provided"""
+
+    def field_name(column_name: str, transform: str):
+        bracket_index = transform.find("[")
+        return f"{column_name}_{transform[:bracket_index] if bracket_index > 0 else transform}"
+
+    if partition_hint is None:
+        return UNPARTITIONED_PARTITION_SPEC
+
+    return PartitionSpec(
+        *(
+            PartitionField(
+                source_id=iceberg_schema.find_field(column_name).field_id,
+                field_id=1000 + index,  # the documentation does this...
+                transform=transforms.parse_transform(transform),
+                name=field_name(column_name, transform),
+            )
+            for index, (column_name, transform) in enumerate(partition_hint.items())
+        )
+    )
--- warehouses/examples/ingest/accelerator/statusdisplay/statusdisplay.py
+++ warehouses/examples/ingest/accelerator/statusdisplay/statusdisplay.py
@@ -8,7 +8,7 @@ Demonstrates the extract contract expected by ``elt run``:
 import datetime
 import random
 import logging
-from typing import Iterator
+from typing import Iterator, Tuple
 
 import httpx
 import pyarrow as pa
@@ -19,31 +19,36 @@ from pyiceberg.exceptions import NoSuchTableError
 
 
 class SourceConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="statusdisplay_")
+    model_config = SettingsConfigDict(env_prefix="statusdisplay__")
 
     api_base_url: str
+    endpoints: list[str]
 
 
-def extract(source_config: SourceConfig, *, backfill: bool = False) -> Iterator:
-    """Pull from the statusdisplay API and"""
-    # resp = httpx.get(source_config.api_url)
-    # resp.raise_for_status()
-    logging.debug(f"Fetching cycle schedule from {source_config.api_base_url}")
+def extract(
+    source_config: SourceConfig, *, backfill: bool = False
+) -> Iterator[Tuple[str, pa.Table]]:
+    """Pull from the statusdisplay API and returned the requested resources"""
+    for endpoint in source_config.endpoints:
+        url = source_config.api_base_url + endpoint
+        logging.debug(f"Fetching cycle schedule from {url}")
+        #        resp = httpx.get(url)
+        #        resp.raise_for_status()
 
-    yield (
-        "schedule",
-        pa.Table.from_pylist(
-            [
-                {
-                    "cycle_name": "2025/1",
-                    "started_at": "2025-01-30T08:30:00",
-                    "ended_at": "2025-02-30T08:30:00",
-                },
-                {
-                    "cycle_name": "2024/1",
-                    "started_at": "2024-01-30T08:30:00",
-                    "ended_at": "2024-02-30T08:30:00",
-                },
-            ]
-        ),
-    )
+        yield (
+            endpoint.lstrip("/"),
+            pa.Table.from_pylist(
+                [
+                    {
+                        "cycle_name": "2025/1",
+                        "started_at": "2025-01-30T08:30:00",
+                        "ended_at": "2025-02-30T08:30:00",
+                    },
+                    {
+                        "cycle_name": "2024/1",
+                        "started_at": "2024-01-30T08:30:00",
+                        "ended_at": "2024-02-30T08:30:00",
+                    },
+                ]
+            ),
+        )
