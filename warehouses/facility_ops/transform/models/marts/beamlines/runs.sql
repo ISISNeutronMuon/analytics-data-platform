43b4e20ade2323c182ba431ada91ac8e0d657bc0
Martyn Gigg <martyn.gigg@gmail.com>
2026-04-20 10:21:06 +0100

    Use microbatch materialization to avoid Iceberg partition limit
    Writing all data at once gives Exceeded limit of 100 open writers for partitions/buckets.

--- warehouses/facility_ops/transform/models/marts/beamlines/runs.sql
+++ warehouses/facility_ops/transform/models/marts/beamlines/runs.sql
@@ -1,8 +1,12 @@
 {{ config(
+    materialized='incremental',
+    incremental_strategy='microbatch',
+    event_time='started_at',
+    begin='2010-01-01',
+    batch_size='year',
     properties={
-      "partitioning": "ARRAY['year(started_at)']",
-    },
-  on_table_exists = 'drop'
+      "partitioning": "ARRAY['instrument', 'year(started_at)']",
+    }
 ) }}
 
 select
--- warehouses/facility_ops/transform/models/marts/beamlines/runs_stats.sql
+++ warehouses/facility_ops/transform/models/marts/beamlines/runs_stats.sql
@@ -7,7 +7,8 @@ select
   instrument,
   count(run_number) as total_runs,
   count(case when event_mode = true then run_number end) as total_event_mode_runs,
-  count(case when event_mode = true then run_number end) * 100.0 / count(run_number) as percentage_event_runs
+  count(case when event_mode = true then run_number end) * 100.0 / count(run_number) as percentage_event_runs,
+  avg(duration_mins) as average_duration_mins
 
 from
   {{ ref('runs') }}
