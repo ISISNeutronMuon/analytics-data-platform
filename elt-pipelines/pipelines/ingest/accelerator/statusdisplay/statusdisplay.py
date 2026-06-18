e4e45926a5050330b6fb91931839a0e78edc5118
Will Taylor <william.h.taylor@stfc.ac.uk>
2026-06-18 17:13:39 +0100

    feat(elt-pipelines): Add statusdisplay pipeline
    ref #321

---
+++ elt-pipelines/pipelines/ingest/accelerator/statusdisplay/statusdisplay.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import io
+import json
+import pyarrow.json
+from pydantic_settings import BaseSettings
+import requests
+
+from elt_common.extract import BaseExtract, ResourceProperties, ResourceWriteProperties
+
+CYCLES_URL = "https://status.isis.stfc.ac.uk/api/cycles"
+
+
+class Extract(BaseExtract):
+    def extract_resource_properties(self):
+        yield (
+            "elt_cycles",
+            ResourceProperties(
+                extractor=extract_cycles,
+                write_properties=ResourceWriteProperties(write_mode="replace"),
+                watermark_column=None
+            )
+        )
+
+
+def extract_cycles(_):
+    data = clean(fetch())
+    newline_delimited = "\n".join(json.dumps(row) for row in data)
+
+    with io.BytesIO(newline_delimited.encode()) as f:
+        yield pyarrow.json.read_json(f)
+
+
+def fetch():
+    response = requests.get(CYCLES_URL)
+    if not response.ok:
+        raise RuntimeError(f"Failed to fetch cycles - {response.reason}")
+
+    return response.json()
+
+
+def reformat(date_string):
+    """Convert a date from ISO format into one that pyarrow will convert into a timestamp"""
+    return datetime.fromisoformat(date_string).strftime("%Y-%m-%d %H:%M:%S")
+
+
+def clean(data):
+    for cycle in data:
+        for phase in cycle["phases"]:
+            if "start" in phase:
+                phase["start"] = reformat(phase["start"])
+            if "end" in phase:
+                phase["end"] = reformat(phase["end"])
+
+    return data
+
+
+if __name__ == "__main__":
+    e = Extract(BaseSettings())
+
+    for t, p in e.extract_resource_properties():
+        extracted = list(p.extractor(None))[0]
+        print(extracted)
