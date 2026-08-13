973f5b57a7c94eb90cadc1443de62ddf54699ac5
Will Taylor <william.h.taylor@stfc.ac.uk>
2026-08-13 12:07:07 +0100

    Apply timezone during parsing
    https://github.com/ISISNeutronMuon/analytics-data-platform/pull/429#discussion_r3767815273
    
    Without the tz information during parse_datetime, the tz of the trino session is used. This means that during daylight savings there would be an hour difference applied to any data collected as UTC
    
    This does now mean there's a problem in the other direction (if the data was collected during daylight savings), but I guess anyone storing daylight savings times without timezones is unlikely to miss an hour here or there.

--- elt-pipelines/facility_ops/transform/macros/parse_utc_timestamp.sql
+++ elt-pipelines/facility_ops/transform/macros/parse_utc_timestamp.sql
@@ -1,5 +1,5 @@
 -- Parse separate date and time strings into a single UTC timestamp
-{%- macro parse_utc_timestamp(date_col, date_format, time_col, time_format='HH:mm:ss', src_timezone='UTC') -%}
+{%- macro parse_utc_timestamp(date_col, date_format, time_col, time_format='HH:mm:ss ZZZ', src_timezone='UTC') -%}
   {{ return(adapter.dispatch('parse_utc_timestamp')(date_col, date_format, time_col, time_format, src_timezone)) }}
 {% endmacro %}
 
@@ -7,7 +7,7 @@
 {%- macro trino__parse_utc_timestamp(date_col, date_format, time_col, time_format, src_timezone) -%}
   with_timezone(
     cast(
-      parse_datetime({{ adapter.quote(date_col) }} || ' ' || {{ adapter.quote(time_col) }},
+      parse_datetime({{ adapter.quote(date_col) }} || ' ' || {{ adapter.quote(time_col) }} || ' {{ src_timezone }}',
                      '{{ date_format ~ ' ' ~ time_format }}')
                      as timestamp(3)
         ),
