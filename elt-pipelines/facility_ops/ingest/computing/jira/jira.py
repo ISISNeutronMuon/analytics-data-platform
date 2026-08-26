67118e3f2428617664c3ba57a9dd819325ad4be5
Andrew Bolt <andrew.bolt@stfc.ac.uk>
2026-08-26 10:42:04 +0100

    implement base settings for credential storage; move extract methods into extract class

--- elt-pipelines/facility_ops/ingest/computing/jira/jira.py
+++ elt-pipelines/facility_ops/ingest/computing/jira/jira.py
@@ -1,9 +1,8 @@
 import enum
-import os
 
 from atlassian import Jira
 from elt_common.extract import BaseExtract, ResourceProperties
-import requests
+from pydantic_settings import BaseSettings
 
 import pyarrow as pa
 import datetime as dt
@@ -21,109 +20,90 @@ class IssueField(enum.StrEnum):
     Teams = "customfield_10591"
 
 
-class Extract(BaseExtract):
+class AtlassianCredentials(BaseSettings):
+    jira_url: str
+    email_address: str
+    api_token: str
+    cloud: bool = True
+
+
+class Extract(BaseExtract[AtlassianCredentials]):
+    config_cls = AtlassianCredentials
+
+    def __init__(self, cfg: AtlassianCredentials):
+        super().__init__(cfg)
+        self._client = Jira(
+            cfg.jira_url, cfg.email_address, cfg.api_token, cloud=cfg.cloud
+        )
+
     def extract_resource_properties(self):
         yield (
             ("all_jira_issues", ResourceProperties(extractor=self.extract_jira_issues)),
         )
 
+    def extract_jira_issues(self) -> pa.Table:
+        project_names = self.extract_isis_project_names()
 
-def _value_or_env_variable(value: str | None, env_var_name: str) -> str:
-    if value is not None:
-        return value
-    else:
-        try:
-            return os.environ[env_var_name]
-        except KeyError:
-            raise KeyError(
-                f"Environment variable '{env_var_name}' not found."
-            ) from None
+        issues = []
+        for project_name in project_names:
+            project_issues = self._client.get_all_project_issues(
+                project_name, fields=[field.value for field in IssueField]
+            )
 
+            for project_issue in project_issues:
+                fields = project_issue["fields"]
 
-def jira_connection(
-    url: str | None = None,
-    email: str | None = None,
-    token: str | None = None,
-    cloud: bool = True,
-) -> Jira:
-    url = _value_or_env_variable(url, "JIRA_URL")
-    email = _value_or_env_variable(email, "EMAIL_ADDRESS")
-    token = _value_or_env_variable(token, "ATLASSIAN_API_TOKEN")
+                created = dt.datetime.strptime(
+                    fields[IssueField.Created], DATE_FORMAT_STRING
+                )
+                updated = dt.datetime.strptime(
+                    fields[IssueField.Updated], DATE_FORMAT_STRING
+                )
 
-    session = requests.Session()
-    jira_connection = Jira(url, email, token, session=session, cloud=cloud)
-    return jira_connection
+                teams = fields.get(IssueField.Teams)
+                team_names = (
+                    [team["value"] for team in teams] if teams is not None else None
+                )
 
+                issues.append(
+                    {
+                        "issue_key": project_issue[IssueField.IssueKey],
+                        "issue_type": fields[IssueField.IssueType]["name"],
+                        "project_name": project_name,
+                        "status": fields[IssueField.Status]["name"],
+                        "priority": fields[IssueField.Priority]["name"],
+                        "created": created,
+                        "updated": updated,
+                        "teams": team_names,
+                    }
+                )
 
-def extract_isis_project_names() -> list[str]:
-    jira_cloud = jira_connection()
-
-    projects = jira_cloud.get_all_projects()
-
-    project_names = []
-    for project in projects:
-        if project["name"] not in project_names:
-            project_names.append(project["name"])
-
-    return list(filter(lambda p: p.startswith("[ISIS]"), project_names))
-
-
-def extract_jira_issues() -> pa.Table:
-    project_names = extract_isis_project_names()
-
-    jira_cloud = jira_connection()
-
-    issues = []
-    for project_name in project_names:
-        project_issues = jira_cloud.get_all_project_issues(
-            project_name, fields=[field.value for field in IssueField]
+        issues_schema = pa.schema(
+            [
+                pa.field("issue_key", pa.string()),
+                pa.field("issue_type", pa.string()),
+                pa.field("project_name", pa.string()),
+                pa.field("status", pa.string()),
+                pa.field("priority", pa.string()),
+                pa.field("created", pa.date32()),
+                pa.field("updated", pa.date32()),
+                pa.field("teams", pa.list_(pa.string())),
+            ]
         )
 
-        for project_issue in project_issues:
-            fields = project_issue["fields"]
+        issues_table = pa.Table.from_pylist(issues, schema=issues_schema)
+        print(issues_table)
+        return issues_table
 
-            created = dt.datetime.strptime(
-                fields[IssueField.Created], DATE_FORMAT_STRING
-            )
-            updated = dt.datetime.strptime(
-                fields[IssueField.Updated], DATE_FORMAT_STRING
-            )
-
-            teams = fields.get(IssueField.Teams)
-            team_names = (
-                [team["value"] for team in teams] if teams is not None else None
-            )
-
-            issues.append(
-                {
-                    "issue_key": project_issue[IssueField.IssueKey],
-                    "issue_type": fields[IssueField.IssueType]["name"],
-                    "project_name": project_name,
-                    "status": fields[IssueField.Status]["name"],
-                    "priority": fields[IssueField.Priority]["name"],
-                    "created": created,
-                    "updated": updated,
-                    "teams": team_names,
-                }
-            )
-
-    issues_schema = pa.schema(
-        [
-            pa.field("issue_key", pa.string()),
-            pa.field("issue_type", pa.string()),
-            pa.field("project_name", pa.string()),
-            pa.field("status", pa.string()),
-            pa.field("priority", pa.string()),
-            pa.field("created", pa.date32()),
-            pa.field("updated", pa.date32()),
-            pa.field("teams", pa.list_(pa.string())),
+    def extract_isis_project_names(self) -> list[str]:
+        projects = self._client.get_all_projects()
+        return [
+            project["name"]
+            for project in projects
+            if project["name"].startswith("[ISIS]")
         ]
-    )
-
-    issues_table = pa.Table.from_pylist(issues, schema=issues_schema)
-    print(issues_table)
-    return issues_table
 
 
 if __name__ == "__main__":
-    extract_jira_issues()
+    extract = Extract(AtlassianCredentials())
+    extract.extract_jira_issues()
