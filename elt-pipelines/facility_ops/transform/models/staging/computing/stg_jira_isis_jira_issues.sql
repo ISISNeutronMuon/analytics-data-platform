c04debc706f6533a191f8e45dbfff37462736c0b
Andrew Bolt <andrew.bolt@stfc.ac.uk>
2026-08-26 16:56:44 +0100

    generalise migrated removal; trim strings of whitespace; format query

--- elt-pipelines/facility_ops/transform/models/staging/computing/stg_jira_isis_jira_issues.sql
+++ elt-pipelines/facility_ops/transform/models/staging/computing/stg_jira_isis_jira_issues.sql
@@ -8,7 +8,14 @@ cleaned as (
 
     select
 
-      issue_key, issue_type, project_name, "status", REPLACE(priority, 'Highest (migrated)', 'Highest') as priority, created as created_at, updated as updated_at, teams
+      TRIM(issue_key) as issue_key,
+      TRIM(issue_type) as issue_type,
+      TRIM(project_name) as project_name,
+      TRIM(REPLACE("status", '(migrated)', '') as "status",
+      TRIM(REPLACE(priority, '(migrated)', '')) as priority,
+      TRIM(created) as created_at,
+      TRIM(updated) as updated_at,
+      teams
 
 
     from source
