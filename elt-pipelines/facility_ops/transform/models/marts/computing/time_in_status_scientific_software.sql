d35b806efe6e9ad7df9607787e8f1a5653f0e96a
Andrew Bolt <andrew.bolt@stfc.ac.uk>
2026-09-04 11:24:02 +0100

    extract time status logic into intermediate group; present report-specific page in mart

--- elt-pipelines/facility_ops/transform/models/marts/computing/time_in_status_scientific_software.sql
+++ elt-pipelines/facility_ops/transform/models/marts/computing/time_in_status_scientific_software.sql
@@ -1,47 +1,7 @@
-
--- 3. Get the differences between the status. No need for final status, so this query is perfectly suitable
-with status_to_from as (
-  select
-    issue_key,
-    from_status as status,
-    lag(changed_at) over (partition by issue_key order by changed_at) as status_from,
-    changed_at as status_to
-    from {{ ref('stg_jira_issue_status_changelogs') }} as changelogs
+with times_in_status as (
+  select * from {{ ref('int_times_in_status') }}
 ),
 
--- 4. Populate null values of status from with the issue creation date. Join required
-nn_status_to_from as (
-  select
-    status_to_from.issue_key,
-    status_to_from.status,
-    COALESCE(status_to_from.status_from, issues.created) as status_from,
-    status_to_from.status_to
-    from status_to_from
-    inner join facility_ops_landing.computing_jira.isis_jira_issues as issues
-    on status_to_from.issue_key = issues.issue_key
-),
-
--- 5. Subtract to and from date
-status_durations as (
-  select
-    issue_key,
-    status,
-    date_diff('second', status_from, status_to) as status_duration
-    from nn_status_to_from
-),
-
--- 6. Aggregate similar statuses and add their durations
-times_in_status as (
-  select
-    issue_key,
-    status,
-    sum(status_duration) as time_in_status
-    from status_durations
-    group by issue_key,
-    status
-),
-
--- 7. Store these in time_in fields. THIS SHOULD BE THE ONLY THING IN THIS PARTICULAR FILE. THE ABOVE SHOULD BE EXTRACTED TO ANOTHER FILE
 time_in_status_scientific_software as (
   select
   issue_key,
