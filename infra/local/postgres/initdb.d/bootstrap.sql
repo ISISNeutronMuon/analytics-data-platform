4e07b8fb9e3f15173f24eca04972c3432f0e4191
Martyn Gigg <martyn.gigg@gmail.com>
2026-09-14 11:26:13 +0100

    Working Airflow after overhaul

--- infra/local/postgres/initdb.d/bootstrap.sql
+++ infra/local/postgres/initdb.d/bootstrap.sql
@@ -1,5 +1,11 @@
 \getenv openfga_db_name OPENFGA_DB_NAME
 \getenv lakekeeper_db_name LAKEKEEPER_DB_NAME
+\getenv airflow_db_name AIRFLOW_DB_NAME
 
+-- Core service databases
 CREATE DATABASE :"openfga_db_name";
 CREATE DATABASE :"lakekeeper_db_name";
+
+-- Optional service databases. Strictly not required by the core
+-- but it's easier to create them here.
+CREATE DATABASE :"airflow_db_name";
