07b360ae95ae356ad818aa78c6abc1ae5560b135
Andrew Bolt <40577677+ambolt314@users.noreply.github.com>
2026-09-16 13:31:06 +0100

    feat(elt-pipelines): Port statusdisplay to elt-pipelines (#470)
    <!---
    Please write the PR title following the conventions at
    https://www.conventionalcommits.org/en/v1.0.0/
    Example:
    fix(elt-common): Fix bug in package
    -->
    
    ### Summary
    
    <!--- Describe the change below, including rationale and design
    decisions -->
    Move `STATUSDISPLAY` staging tables from `warehouses` to `elt-pipelines`
    
    Ref #458 . <!-- and fix #xxxx or close #xxxx xor resolves #xxxx. One
    line per issue fixed. -->
    <!-- alternative
     *There is no associated issue.*
    -->

---
+++ elt-pipelines/facility_ops/transform/models/staging/accelerator/stg_statusdisplay__cycles.sql
@@ -0,0 +1,17 @@
+with
+source as (
+    select * from {{source('accelerator_statusdisplay', 'elt_cycles')}}
+),
+
+status_display as (
+    select
+        source.label as {{ adapter.quote("name") }},
+        cast(json_extract_scalar(phase, '$.start') as timestamp(6)) at time zone 'UTC' as started_at,
+        cast(json_extract_scalar(phase, '$.end') as timestamp(6)) at time zone 'UTC' as ended_at,
+        json_extract_scalar(phase, '$.type') as phase,
+        cast(json_extract_scalar(phase, '$.target') as int) as {{ adapter.quote("target") }}
+        from source
+        cross join unnest(cast(source.phases as array(json))) as u(phase)
+)
+
+select * from status_display
