f2318214a45783c93cc707e88df1dd84c99234cf
Andrew Bolt <andrew.bolt@stfc.ac.uk>
2026-09-16 11:45:35 +0100

    added missing time zone specification, referencing https://github.com/ISISNeutronMuon/analytics-data-platform/pull/470#discussion_r4025072624

--- elt-pipelines/facility_ops/transform/models/staging/accelerator/stg_statusdisplay__cycles.sql
+++ elt-pipelines/facility_ops/transform/models/staging/accelerator/stg_statusdisplay__cycles.sql
@@ -7,7 +7,7 @@ status_display as (
     select
         source.label as {{ adapter.quote("name") }},
         cast(json_extract_scalar(phase, '$.start') as timestamp(6)) at time zone 'UTC' as started_at,
-        cast(json_extract_scalar(phase, '$.end') as timestamp(6)) as ended_at,
+        cast(json_extract_scalar(phase, '$.end') as timestamp(6)) at time zone 'UTC' as ended_at,
         json_extract_scalar(phase, '$.type') as phase,
         cast(json_extract_scalar(phase, '$.target') as int) as {{ adapter.quote("target") }}
         from source
