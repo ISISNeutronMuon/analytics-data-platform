2397df06aee19dfc7a4415a160735f69ab54383f
Andrew Bolt <andrew.bolt@stfc.ac.uk>
2026-09-16 10:21:37 +0100

    add explicit casting; avoid reserved keywords

--- elt-pipelines/facility_ops/transform/models/staging/accelerator/stg_statusdisplay__cycles.sql
+++ elt-pipelines/facility_ops/transform/models/staging/accelerator/stg_statusdisplay__cycles.sql
@@ -5,11 +5,11 @@ source as (
 
 status_display as (
     select
-        source.label as name,
-        json_extract_scalar(phase, '$.start') as started_at,
-        json_extract_scalar(phase, '$.end') as ended_at,
+        source.label as {{ adapter.quote("name") }},
+        cast(json_extract_scalar(phase, '$.start') as timestamp) as started_at,
+        cast(json_extract_scalar(phase, '$.end') as timestamp) as ended_at,
         json_extract_scalar(phase, '$.type') as phase,
-        json_extract_scalar(phase, '$.target') as target
+        cast(json_extract_scalar(phase, '$.target') as int) as {{ adapter.quote("target") }}
         from source
         cross join unnest(cast(source.phases as array(json))) as u(phase)
 )
