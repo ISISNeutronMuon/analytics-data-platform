a3103a5a8121092e2b2287ca7d5e5543929e0f4f
Will Taylor <william.h.taylor@stfc.ac.uk>
2026-09-17 17:05:22 +0100

    Add electricity_sharepoint ingest tests
    ref #443

---
+++ elt-pipelines/facility_ops/ingest/estates/electricity_sharepoint/test_electricity_sharepoint.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+
+from elt_common.testing.pipelines import AssertableCatalog
+
+_namespace = "estates_electricity_sharepoint"
+_table_id = (_namespace, "rdm_data")
+
+
+def test_expected_columns_created(test_catalog: AssertableCatalog, run_test_ingest):
+    run_test_ingest()
+    test_catalog.assert_has_columns(
+        _table_id,
+        [
+            "date_time",
+            "isis_elec_total_power_mw",
+            "file_name",
+        ],
+    )
+    test_catalog.assert_at_least_rows(_table_id, 100)
+
+    arrow_table = test_catalog.catalog.load_table(_table_id).scan().to_arrow()
+
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    for d in arrow_table["date_time"].to_pylist():
+        diff = now - d
+        assert diff.days == 0, (
+            "Default electricity_sharepoint values should all be from today"
+        )
+
+
+def test_backfill_globs_xlsx(test_catalog: AssertableCatalog, run_test_ingest):
+    run_test_ingest(
+        backfill="True", backfill_globs=json.dumps(["**/JAN2025 ISIS.xlsx"])
+    )
+    # Historical data shouldn't change so we should get exactly the correct number of rows
+    test_catalog.assert_has_n_rows(_table_id, 34719)
+
+
+def test_backfill_globs_daily_csv(test_catalog: AssertableCatalog, run_test_ingest):
+    run_test_ingest(backfill="True", backfill_globs=json.dumps(["**/250708-daily.csv"]))
+    # Historical data shouldn't change so we should get exactly the correct number of rows
+    test_catalog.assert_has_n_rows(_table_id, 764)
